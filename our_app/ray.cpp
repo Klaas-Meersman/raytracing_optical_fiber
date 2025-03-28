93ec756a1a82940e96fe082270f843a1905ad836
Klaas Meersman <klaas.meersman@live.be>
2025-03-28 14:59:27 +0100

    started ray class

---
+++ our_app/ray.cpp
@@ -0,0 +1,24 @@
+#include "ray.hpp"
+#include <cmath>
+
+
+
+
+
+//constructor def
+Co::Co(int x, int y):x(x),y(y){
+    //this->x = x;
+}
+
+
+Co::~Co(){
+}
+
+
+
+Co& Co::getCo() {
+    return *this;
+}
+
+
+
--- our_app/ray.hpp
+++ our_app/ray.hpp
@@ -5,7 +5,21 @@
 
 class Ray
 {
+
+
 private:
-    Co start
-    
-}
+    Co start;
+
+    //we assume the angle between the starting point 
+    //and the endpoint of an arrow point to the right
+    //simply: as it would be in a goniometric circle
+    int angleOfDeparture;
+
+
+
+    std::vector<int> generateStraightPath(int step);
+
+
+
+
+};
