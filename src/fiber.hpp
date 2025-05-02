452e8863969cb1b4648f7efb079845a5e5312e4c
Klaas Meersman <klaas.meersman@live.be>
2025-05-02 18:19:32 +0200

    add fiber

---
+++ src/fiber.hpp
@@ -0,0 +1,19 @@
+#include <vector>
+#include "coordinate.hpp"
+
+
+class Fiber
+{
+private:
+    const double_t topY, bottomY, width, length;
+
+public:
+    //constructor
+    Fiber(double_t topY, double_t bottomY, double_t width, double_t length);
+    //destructor
+     ~Fiber();
+    //this would typically be no longer than width + length of the fiber it is in
+    inline int Fiber::maxSingleRayInFiber(){
+        return width+length;
+    }
+};
